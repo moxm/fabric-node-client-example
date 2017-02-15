4e448ba0d5ab3ec66d6f032b883ebdd05d554a45
Richard <richard.ma@msn.com>
2017-02-15 13:23:09 +0800

    first commit

---
+++ routes/get.js
@@ -0,0 +1,109 @@
+
+
+/**
+ * Created by Neel on 2017/2/14.
+ */
+'use strict';
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var log4js = require('log4js');
+var logger = log4js.getLogger('QUERY');
+
+var hfc = require('fabric-client');
+var utils = require('fabric-client/lib/utils.js');
+var Peer = require('fabric-client/lib/Peer.js');
+var Orderer = require('fabric-client/lib/Orderer.js');
+
+var config = require('./config.json');
+var helper = require('./helper.js');
+
+var express = require('express');
+var router = express.Router();
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: false }));
+
+logger.setLevel('DEBUG');
+
+
+var client = new hfc();
+var chain;
+
+router.get('/map/:key', function (req, res) {
+    // res.send('Hello World!')
+    console.log("get > key: " + req.params.key);
+    console.log(req.body);
+    if (!chain) {
+        init();
+    }
+    query(req, res);
+})
+/*
+router.post('/map/', function (req, res) {
+    // res.send('Hello World!')
+    console.log("put");
+    console.log(req.body.key);
+    console.log(req.body.value);
+    res.send(req.body);
+})
+router.delete('/map/:key', function (req, res) {
+    console.log("delete > key: " + req.params.key);
+    res.send("delete > key: " + req.params.key);
+})
+
+function init() {
+    chain = client.newChain(config.chainName);
+    chain.addOrderer(new Orderer(config.orderer.orderer_url));
+    for (var i = 0; i < config.peers.length; i++) {
+        chain.addPeer(new Peer(config.peers[i].peer_url));
+    }
+}*/
+
+
+function query(key, res) {
+    hfc.newDefaultKeyValueStore({
+        path: config.keyValueStore
+    }).then(function(store) {
+        client.setStateStore(store);
+        return helper.getSubmitter(client);
+    }).then(function(admin) {
+        logger.info('Successfully obtained enrolled user to perform query');
+
+        logger.info('Executing Query');
+        let targets = [];
+        for (var i = 0; i < config.peers.length; i++) {
+            targets.push(config.peers[i]);
+        }
+        let args = ["query", key.params.key];
+
+        //chaincode query request
+        let request = {
+            targets: targets,
+            chaincodeId: config.chaincodeID,
+            chainId: config.channelID,
+            txId: utils.buildTransactionID(),
+            nonce: utils.getNonce(),
+            fcn: config.queryRequest.functionName,
+            args: args
+        };
+        // Query chaincode
+        return chain.queryByChaincode(request);
+    }).then(function(response_payloads) {
+        for (let i = 0; i < response_payloads.length; i++) {
+            logger.info('############### Query results after the move on PEER%j, User "b" now has  %j', i, response_payloads[i].toString('utf8'));
+        }
+
+        if (response_payloads.length == 0) {
+            res.send({code: 'ERROR', message: '值不存在'});
+        } else {
+            res.send({code: 'SUCCESS', message: '查询成功', body: response_payloads[0].toString('utf8')});
+        }
+    }).catch(function(err) {
+        res.send({code: 'ERROR', message: '查询异常'});
+            logger.error('Failed to end to end test with error:' + err.stack ? err.stack : err);
+    });
+}
+
+
+module.exports = router;
---
+++ routes/put.js
@@ -0,0 +1,101 @@
+
+
+/**
+ * Created by Neel on 2017/2/14.
+ */
+'use strict';
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var log4js = require('log4js');
+var logger = log4js.getLogger('INVOKE');
+
+var hfc = require('fabric-client');
+var utils = require('fabric-client/lib/utils.js');
+var Peer = require('fabric-client/lib/Peer.js');
+var Orderer = require('fabric-client/lib/Orderer.js');
+var EventHub = require('fabric-client/lib/EventHub.js');
+
+var config = require('./../config.json');
+var helper = require('./helper.js');
+
+var express = require('express');
+var router = express.Router();
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: false }));
+
+logger.setLevel('DEBUG');
+
+
+var client = new hfc();
+var chain;
+var eventhub;
+var tx_id = null;
+
+router.post('/map/', function (req, res) {
+    // res.send('Hello World!')
+    console.log("put");
+    console.log(req.body.key);
+    console.log(req.body.value);
+    if (!chain) {
+        init();
+    }
+    put(req, res);
+})
+
+function init() {
+    chain = client.newChain(config.chainName);
+    chain.addOrderer(new Orderer(config.orderer.orderer_url));
+    eventhub = new EventHub();
+    eventhub.setPeerAddr(config.events[0].event_url);
+    eventhub.connect();
+    for (let i = 0; i < config.peers.length; i++) {
+        chain.addPeer(new Peer(config.peers[i].peer_url));
+    }
+}
+function put(req, res) {
+    hfc.newDefaultKeyValueStore({
+        path: config.keyValueStore
+    }).then(function(store) {
+        client.setStateStore(store);
+        return helper.getSubmitter(client);
+    }).then(function(admin) {
+        logger.info('Successfully obtained user to submit transaction');
+
+        logger.info('Executing Invoke');
+        tx_id = helper.getTxId();
+        let nonce = utils.getNonce();
+
+        let args = ["put", req.body.key, req.body.value];
+        // send proposal to endorser
+        let request = {
+            chaincodeId: config.chaincodeID,
+            fcn: config.deleteRequest.functionName,
+            args: args,
+            chainId: config.channelID,
+            txId: tx_id,
+            nonce: nonce
+        };
+        return chain.sendTransactionProposal(request);
+    }).then(function(results) {
+        logger.info('Successfully obtained proposal responses from endorsers');
+
+        return helper.processProposal(tx_id, eventhub, chain, results, 'put');
+    }).then(function(response) {
+        if (response.status === 'SUCCESS') {
+            res.send({code: 'SUCCESS', message: '保存成功'});
+            logger.info('The chaincode transaction has been successfully committed');
+            process.exit();
+        } else {
+            res.send({code: 'ERROR', message: '保存失败'});
+        }
+    }).catch(function(err) {
+        res.send({code: 'ERROR', message: '保存异常'});
+        eventhub.disconnect();
+        logger.error('Failed to invoke transaction due to error: ' + err.stack ? err.stack : err);
+    });
+}
+
+
+module.exports = router;
